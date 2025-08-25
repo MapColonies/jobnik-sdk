2ace8d4b7726d4a846ca82386180c48f696dd8cc
Schnitz <12687466+CptSchnitz@users.noreply.github.com>
2025-08-25 15:52:49 +0300

    chore: wip

--- src/clients/worker.ts
+++ src/clients/worker.ts
@@ -1,16 +1,39 @@
 import { setTimeout as sleep } from 'node:timers/promises';
-import circuitBreaker from 'opossum';
+import circuitBreaker, { Options as OpossumOptions } from 'opossum';
+import { context, propagation, Span, SpanKind, SpanStatusCode, trace } from '@opentelemetry/api';
 import { ApiClient, ScopedApiClient } from '../api';
 import { Logger } from '../types';
 import { StageData } from '../types/stage';
 import { getErrorMessageFromUnknown, presult } from '../common/utils';
 import { Task } from '../types/task';
+import {
+  ATTR_JOB_MANAGER_STAGE_ID,
+  ATTR_MESSAGING_BATCH_MESSAGE_COUNT,
+  ATTR_MESSAGING_DESTINATION_NAME,
+  ATTR_MESSAGING_MESSAGE_ID,
+} from '../telemetry/semconv';
+import { BASE_ATTRIBUTES, tracer } from '../telemetry/trace';
 import { Producer } from './producer';
 import { Consumer } from './consumer';
+
+const DEFAULT_PULLING_INTERVAL = 10000;
+
+const defaultCircuitBreakerOptions = {
+  enabled: true,
+  rollingCountTimeout: 60000,
+  errorThresholdPercentage: 50,
+  resetTimeout: 30000,
+  timeout: false,
+  allowWarmUp: true,
+} satisfies OpossumOptions;
+
+export type CircuitBreakerOptions = Pick<OpossumOptions, 'enabled' | 'rollingCountTimeout' | 'errorThresholdPercentage' | 'resetTimeout'>;
 
 export interface WorkerOptions {
   concurrency?: number;
   pullingInterval?: number;
+  taskHandlerCircuitBreaker?: CircuitBreakerOptions;
+  dequeueTaskCircuitBreaker?: CircuitBreakerOptions;
 }
 
 export interface TaskHandlerContext {
@@ -23,12 +46,13 @@ export interface TaskHandlerContext {
 export type TaskHandler = (task: Task, context: TaskHandlerContext) => Promise<void>;
 
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
-export class Worker<StageTypes extends { [K in keyof StageTypes]: StageData } = {}> {
+export class Worker<StageTypes extends { [K in keyof StageTypes]: StageData } = {}> extends Consumer<StageTypes> {
   private readonly abortController = new AbortController();
   private readonly taskHandlerCircuitBreaker: circuitBreaker<[Task, TaskHandlerContext]>;
   private taskCircuitBreakerPromise: Promise<void> | null = null;
   private taskCircuitBreakerResolve: ((value: void) => void) | null = null;
   private readonly dequeueTaskCircuitBreaker: circuitBreaker<[string], Task | null>;
+  private readonly taskHandlerCbResetTimeout: number;
 
   // queue members
   private isRunning: boolean = false;
@@ -36,25 +60,26 @@ export class Worker<StageTypes extends { [K in keyof StageTypes]: StageData } = 
 
   public constructor(
     private readonly taskHandler: TaskHandler,
+    private readonly stageType: string,
     private readonly options: WorkerOptions,
-    private readonly logger: Logger,
-    private readonly apiClient: ApiClient,
-    private readonly consumer: Consumer,
-    private readonly stageType: string
+    logger: Logger,
+    apiClient: ApiClient
   ) {
+    super(apiClient, logger);
     this.taskHandlerCircuitBreaker = new circuitBreaker(this.taskHandler, {
-      timeout: 6000,
-      errorThresholdPercentage: 50,
-      resetTimeout: 30000,
+      ...defaultCircuitBreakerOptions,
+      ...options.taskHandlerCircuitBreaker,
       name: 'taskHandler',
     });
 
+    this.taskHandlerCbResetTimeout = options.taskHandlerCircuitBreaker?.resetTimeout ?? defaultCircuitBreakerOptions.resetTimeout;
+
     this.setupTaskHandlerCircuitBreaker();
 
-    this.dequeueTaskCircuitBreaker = new circuitBreaker(this.consumer.dequeueTask.bind(this), {
+    this.dequeueTaskCircuitBreaker = new circuitBreaker(this.dequeueTask.bind(this), {
+      ...defaultCircuitBreakerOptions,
+      ...options.dequeueTaskCircuitBreaker,
       name: 'dequeueTask',
-      errorThresholdPercentage: 50,
-      resetTimeout: 30000,
     });
 
     this.setupDequeueCircuitBreaker();
@@ -68,14 +93,14 @@ export class Worker<StageTypes extends { [K in keyof StageTypes]: StageData } = 
 
     this.isRunning = true;
 
-    for await (const task of this.taskIterator()) {
+    for await (const [task, span] of this.taskIterator()) {
       this.logger.debug('Dequeued task for processing', {
         taskId: task.id,
         runningTasks: this.runningTasks.size,
         stageType: this.stageType,
       });
 
-      this.runTask(task);
+      this.runTask(task, span);
 
       while (this.runningTasks.size >= (this.options.concurrency ?? 1)) {
         this.logger.debug('Concurrency limit reached', {
@@ -102,9 +127,9 @@ export class Worker<StageTypes extends { [K in keyof StageTypes]: StageData } = 
     this.logger.info('Worker stopped', { stageType: this.stageType });
   }
 
-  private runTask(task: Task): void {
+  private runTask(task: Task, span: Span): void {
     this.logger.info('Running task', { taskId: task.id, stageType: this.stageType });
-    const context = {
+    const taskContext = {
       signal: this.abortController.signal,
       logger: this.logger,
       producer: new Producer(this.apiClient, this.logger),
@@ -112,24 +137,39 @@ export class Worker<StageTypes extends { [K in keyof StageTypes]: StageData } = 
     };
 
     const wrapHandler = async (): Promise<void> => {
-      try {
-        this.logger.debug('Task handler firing', {
-          taskId: task.id,
-          stageType: this.stageType,
-        });
+      const ctx = trace.setSpan(context.active(), span);
+      await context.with(ctx, async () => {
+        try {
+          this.logger.debug('Task handler firing', {
+            taskId: task.id,
+            stageType: this.stageType,
+          });
 
-        await this.taskHandlerCircuitBreaker.fire(task, context);
+          await this.taskHandlerCircuitBreaker.fire(task, taskContext);
+          this.logger.debug('Task handler succeeded', { taskId: task.id, stageType: this.stageType });
+          await this.markTask('COMPLETED', { task: task as Task });
+        } catch (error) {
+          this.logger.warn('Task handler failed', {
+            taskId: task.id,
+            stageType: this.stageType,
+            error: getErrorMessageFromUnknown(error),
+          });
 
-        this.logger.debug('Task handler succeeded', { taskId: task.id, stageType: this.stageType });
-        await this.consumer.markTaskCompleted(task.id);
-      } catch (error) {
-        this.logger.warn('Task handler failed', {
-          taskId: task.id,
-          stageType: this.stageType,
-          error: getErrorMessageFromUnknown(error),
-        });
-        await this.consumer.markTaskFailed(task.id);
-      }
+          const [err] = await presult(this.markTaskFailed(task.id));
+
+          if (err) {
+            this.logger.error('Error occurred while marking task as failed', {
+              taskId: task.id,
+              stageType: this.stageType,
+              error: getErrorMessageFromUnknown(err),
+            });
+          }
+
+          span.recordException(error as Error);
+          span.setStatus({ code: SpanStatusCode.ERROR });
+          span.end();
+        }
+      });
     };
 
     const taskPromise = wrapHandler()
@@ -152,22 +192,39 @@ export class Worker<StageTypes extends { [K in keyof StageTypes]: StageData } = 
     this.runningTasks.add(taskPromise);
   }
 
-  private async *taskIterator(): AsyncGenerator<Task> {
+  private async *taskIterator(): AsyncGenerator<[Task, Span]> {
     while (this.isRunning) {
       if (this.taskCircuitBreakerPromise) {
         this.logger.debug('Waiting for task circuit breaker', {
           stageType: this.stageType,
         });
 
-        await Promise.race([this.taskCircuitBreakerPromise, sleep(30000)]);
+        await Promise.race([this.taskCircuitBreakerPromise, sleep(this.taskHandlerCbResetTimeout)]);
       }
 
-      const [err, task] = await presult(this.dequeueTaskCircuitBreaker.fire(this.stageType));
+      const span = tracer.startSpan(`process ${this.stageType}`, {
+        root: true,
+        kind: SpanKind.CONSUMER,
+        attributes: {
+          [ATTR_MESSAGING_DESTINATION_NAME]: this.stageType,
+          ...BASE_ATTRIBUTES,
+        },
+      });
+
+      const ctx = trace.setSpan(context.active(), span);
+
+      const [err, task] = await context.with(ctx, async () => presult(this.dequeueTaskCircuitBreaker.fire(this.stageType)));
+
       if (err) {
         this.logger.debug('Error while dequeuing task', {
           stageType: this.stageType,
           error: getErrorMessageFromUnknown(err),
         });
+
+        span.recordException(err);
+        span.setStatus({ code: SpanStatusCode.ERROR });
+        span.setAttribute(ATTR_MESSAGING_BATCH_MESSAGE_COUNT, 0);
+        span.end();
       }
 
       if (task) {
@@ -175,10 +232,26 @@ export class Worker<StageTypes extends { [K in keyof StageTypes]: StageData } = 
           taskId: task.id,
           stageType: this.stageType,
         });
-        yield task;
+
+        const remoteContext = propagation.extract(context.active(), task);
+        const taskSpanContext = trace.getSpanContext(remoteContext);
+
+        if (taskSpanContext) {
+          span.addLink({ context: taskSpanContext });
+        }
+
+        span.setAttributes({
+          [ATTR_JOB_MANAGER_STAGE_ID]: task.stageId,
+          [ATTR_MESSAGING_MESSAGE_ID]: task.id,
+        });
+
+        yield [task, span];
+      } else {
+        span.setAttribute(ATTR_MESSAGING_BATCH_MESSAGE_COUNT, 0);
+        span.end();
       }
 
-      await sleep(1000);
+      await sleep(this.options.pullingInterval ?? DEFAULT_PULLING_INTERVAL);
     }
   }
 
